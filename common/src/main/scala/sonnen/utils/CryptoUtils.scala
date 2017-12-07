3dda208fc3ba4a0d61fafd8133db5203108eee4b
zoltanmaric <zoltanmaric@protonmail.com>
2017-12-07 01:37:33 +0100

    extract CryptoUtils

---
+++ common/src/main/scala/sonnen/utils/CryptoUtils.scala
@@ -0,0 +1,46 @@
+package sonnen.utils
+
+import java.security.spec.X509EncodedKeySpec
+import java.security.{KeyFactory, PrivateKey, PublicKey, Signature}
+import java.util.Base64
+
+
+object CryptoUtils {
+
+  private val SignatureImplementation = "SHA256withRSA"
+  private val KeyPairImplementation = "RSA"
+
+  def sign(bytes: Array[Byte], privateKey: PrivateKey): String = {
+    val signer = Signature.getInstance(SignatureImplementation)
+    signer.initSign(privateKey)
+    signer.update(bytes)
+
+    val signature = signer.sign()
+    toBase64(signature)
+  }
+
+  def verifySignature(bytes: Array[Byte], base64PublicKey: String, base64Signature: String): Boolean = {
+
+    val publicKey = toPublicKey(base64PublicKey)
+
+    val sig = Signature.getInstance(SignatureImplementation)
+    sig.initVerify(publicKey)
+    sig.update(bytes)
+
+    sig.verify(toByteArray(base64Signature))
+  }
+
+  def toBase64(publicKey: PublicKey): String =
+    toBase64(publicKey.getEncoded)
+
+  private def toBase64(bytes: Array[Byte]): String =
+    Base64.getEncoder.encodeToString(bytes)
+
+  private def toPublicKey(base64PublicKey: String): PublicKey = {
+    val publicKeyBytes = toByteArray(base64PublicKey)
+    KeyFactory.getInstance(KeyPairImplementation).generatePublic(new X509EncodedKeySpec(publicKeyBytes))
+  }
+
+  private def toByteArray(base64String: String): Array[Byte] =
+    Base64.getDecoder.decode(base64String)
+}
--- common/src/main/scala/sonnen/utils/Signer.scala
+++ common/src/main/scala/sonnen/utils/Signer.scala
@@ -18,11 +18,8 @@ object Signer {
   def sign(reading: Reading, keyPair: KeyPair): SignedReading = {
     val dataToSign = toByteArray(reading)
 
-    val sig = Signature.getInstance("SHA256withRSA")
-    sig.initSign(keyPair.getPrivate)
-    sig.update(dataToSign)
-    val base64Signature = Base64.getEncoder.encodeToString(sig.sign())
-    val base64PublicKey = Base64.getEncoder.encodeToString(keyPair.getPublic.getEncoded)
+    val base64Signature = CryptoUtils.sign(dataToSign, keyPair.getPrivate)
+    val base64PublicKey = CryptoUtils.toBase64(keyPair.getPublic)
 
     SignedReading(base64PublicKey, base64Signature, reading)
   }
@@ -30,14 +27,7 @@ object Signer {
   def verifySignature(signedReading: SignedReading): Boolean = {
     val dataToSign = toByteArray(signedReading.reading)
 
-    val publicKeyBytes = Base64.getDecoder.decode(signedReading.base64PublicKey)
-    val publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publicKeyBytes))
-
-    val sig = Signature.getInstance("SHA256withRSA")
-    sig.initVerify(publicKey)
-    sig.update(dataToSign)
-
-    sig.verify(Base64.getDecoder.decode(signedReading.base64Signature))
+    CryptoUtils.verifySignature(dataToSign, signedReading.base64PublicKey, signedReading.base64Signature)
   }
 
   private def toByteArray(reading: Reading): Array[Byte] = {
