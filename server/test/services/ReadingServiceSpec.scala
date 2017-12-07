b2e54c562cee2d909d62f728e6764c6c06be3b54
zoltanmaric <zoltanmaric@protonmail.com>
2017-12-08 00:05:09 +0100

    add tests for possible attacks

--- server/test/services/ReadingServiceSpec.scala
+++ server/test/services/ReadingServiceSpec.scala
@@ -32,5 +32,59 @@ class ReadingServiceSpec extends AsyncWordSpecLike with MockitoSugar with Matche
       service.verifyReading(signedReading).map(_ shouldEqual VerificationSuccess)
     }
 
+    "reject reading with valid signature from unknown user" in {
+      val service = new TestService()
+
+      val keyPair = CryptoUtils.generateKeyPair()
+      val base64PublicKey = CryptoUtils.toBase64(keyPair.getPublic)
+
+      // Mock DAO says this is not a known public key
+      when(service.dao.publicKeyExists(base64PublicKey)) thenReturn Future.successful(false)
+
+      val reading = Reading(1, 2, Instant.now.toEpochMilli)
+
+      val signedReading = Signer.sign(reading, keyPair)
+
+      service.verifyReading(signedReading).map(_ shouldBe a[VerificationFailure])
+    }
+
+    "reject reading with invalid signature from known user" in {
+      val service = new TestService()
+
+      val keyPair = CryptoUtils.generateKeyPair()
+      val base64PublicKey = CryptoUtils.toBase64(keyPair.getPublic)
+
+      when(service.dao.publicKeyExists(base64PublicKey)) thenReturn Future.successful(true)
+
+      val reading = Reading(1, 2, Instant.now.toEpochMilli)
+
+      val signedReading = Signer.sign(reading, keyPair)
+
+      // Corrupt the character at index 2
+      val corruptedBase64Signature = signedReading.base64Signature.updated(2, 'Y')
+
+      // Replace signature with the corrupted one
+      val corruptedSignedReading = signedReading.copy(base64Signature = corruptedBase64Signature)
+
+      service.verifyReading(corruptedSignedReading).map(_ shouldBe a[VerificationFailure])
+    }
+
+    "reject reading with tampered reading from known user" in {
+      val service = new TestService()
+
+      val keyPair = CryptoUtils.generateKeyPair()
+      val base64PublicKey = CryptoUtils.toBase64(keyPair.getPublic)
+
+      when(service.dao.publicKeyExists(base64PublicKey)) thenReturn Future.successful(true)
+
+      val reading = Reading(1, 2, Instant.now.toEpochMilli)
+
+      val signedReading = Signer.sign(reading, keyPair)
+
+      // Replace reading in SignedReading
+      val tamperedSignedReading = signedReading.copy(reading = Reading(1000, 1000, reading.timestamp))
+
+      service.verifyReading(tamperedSignedReading).map(_ shouldBe a[VerificationFailure])
+    }
   }
 }
